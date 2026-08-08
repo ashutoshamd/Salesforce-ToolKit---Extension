efd00e366f7b80a41c4786c9022577c49b931d4d
ashutoshamd <Ashutosh.Dubey@amdocs.com>
2026-08-08 17:27:21 +0530

    Enhance extension.js with descriptive comments
    Added comments to clarify code functionality and purpose.

--- extension.js
+++ extension.js
@@ -1,21 +1,21 @@
-const vscode = require('vscode');
+const vscode = require('vscode'); // Give my JavaScript access to VS Code. Without this:  vscode.window wouldn't exist. require('vscode') -> VS Code APIs
 
-function activate(context) {
+function activate(context) {  // I'm ready. Tell me what you want me to do. Extension loaded -> activate() --> register functionality
 
-    const command = vscode.commands.registerCommand(
+    const command = vscode.commands.registerCommand( // Register Command. This needs to match the command ID declared in package.json. Button -> Event -> Handler
         'salesforce-toolkit.open',
         function () {
 
-            const panel = vscode.window.createWebviewPanel(
-                'salesforceToolkit',
-                'Salesforce Toolkit',
-                vscode.ViewColumn.One,
+            const panel = vscode.window.createWebviewPanel(  // Create a webpage-like panel inside VS Code.
+                'salesforceToolkit',  // 'salesforceToolkit', is the internal ID
+                'Salesforce Toolkit',  // is the title shown on the tab.
+                vscode.ViewColumn.One, // 
                 {
                     enableScripts: true
-                }
+                }  // Allow JavaScript inside our webview. VS Code webviews have scripting disabled by default, so we explicitly enable it because our UI will eventually need JavaScript to respond to radio buttons and the Execute button.
             );
 
-            panel.webview.html = getWebviewContent();
+            panel.webview.html = getWebviewContent(); // Here's the HTML that I want you to display.
 
         }
     );
